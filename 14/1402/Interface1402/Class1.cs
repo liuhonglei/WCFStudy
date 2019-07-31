d51cef0ea898023bf50f0ea3669f00c541768847
liuhonglei <233624406@qq.com>
2019-07-31 17:46:01 +0800

    7.31

---
+++ 14/1402/Interface1402/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Interface1402
+{
+    [ServiceContract(Namespace = "http://www.lhl.com/bankingservice")]
+    public interface  IBankingService
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void Transfer(string accountFrom, string accountTo, double amount);
+    }
+    [ServiceContract(Namespace = "http://www.lhl.com/bankingservice")]
+    public interface IWithDrawService
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void WithDraw(string accountId, double amount);
+
+    }
+
+    [ServiceContract(Namespace = "http://www.lhl.com/bankingservice")]
+    public interface IDepositService
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void Deposit(string accountId, double amount);
+
+    }
+}
