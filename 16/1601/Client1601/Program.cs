61529a083bcab1a42b3795820875d14c663d8abd
liuhonglei <233624406@qq.com>
2019-08-13 14:18:02 +0800

    dev

---
+++ 16/1601/Client1601/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace Client1601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public  class MessageInspector {
+
+        public int DropRate { get; private set; }
+        public Random Randomizer { get; private set; }
+        public MessageInspector(int dropRate) {
+            this.DropRate = dropRate;
+            Randomizer = new Random();
+        }
+
+        public virtual void ProcessMessage(ref Message message) {
+            int randomNumber = this.Randomizer.Next(100);
+            if (randomNumber <= this.DropRate)
+                message = null;
+        }
+    }
+}
